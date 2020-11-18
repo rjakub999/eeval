dd616495f9d4f12b56012b4dad0eb4cbfdffe525
Jakub Rudek <rjakub999@gmail.com>
2020-11-18 21:38:30 +0100

    Uzupełnienie licencji i dokumentacji

--- R/eeval_calc.R
+++ R/eeval_calc.R
@@ -1,3 +1,12 @@
+#' Funkcja mnozy dwa argumrnty
+#'
+#' @param x double
+#' @param y double
+#'
+#' @return double
+#' @export
+#'
+#' @examples eeval_calc(2, 4)
 eeval_calc <- function(x, y){
   return(x*y)
 }
