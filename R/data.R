532048e0afe4cd5ade79f6f4a6e8822df5457b61
Jakub Rudek <rjakub999@gmail.com>
2020-11-19 12:51:49 +0100

    Dodanie pliku z danymi

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Dane potrzebne dla funkcji szacowania emisji
+#'
+#' @source Plik emisja2019.xlsx
+#' @format Ramka danych zawiera:
+#' \describe{
+#'  \item{Category}{Passenger Cars itd.}
+#'  \item{Fuel}{Petrol itd.}
+#' }
+#' @examples
+#' \dontrun{
+#'  wskazniki
+#' }
+"wskazniki"
