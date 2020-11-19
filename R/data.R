055f53dba93202b7687280c22588f6f48393eef1
Jakub Rudek <rjakub999@gmail.com>
2020-11-19 20:13:54 +0100

    Dodanie opisu do input

--- R/data.R
+++ R/data.R
@@ -12,4 +12,17 @@
 #'  input
 #' }
 "wskazniki"
+#' Dane potrzebne dla funkcji szacowania emisji
+#'
+#' @source Wylosowane dane
+#' @format Ramka danych zawiera:
+#' \describe{
+#'  \item{Nat}{Natezenie ruchu}
+#'  \item{Segment}{Jak we wskazniki}
+#'  \item{Fuel}{Jak we wskazniki}
+#'  \item{Technology}{Jak we wskazniki}
+#' }
+#' @examples
+#' \dontrun{
+#'  input
 "input"
