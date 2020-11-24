00a426985f1b55686d9d0a573dccb8b199f64818
Jakub Rudek <rjakub999@gmail.com>
2020-11-24 21:47:07 +0100

    Dodanie dokumentacji pakietu

---
+++ R/eeval.R
@@ -0,0 +1,13 @@
+#' eeval:Pakiet oceny emisji (emission evaluation)
+#'
+#' Pakiet zawiera dwie funkcje:
+#' eeval_calc oblicza emisje
+#' eeval_draw rysuje wykresy emisji
+#'
+#' @section Dane do obliczen: Funkcje pakietu korzystaja z danych dolaczonych do pakietu.
+#'   Dane sa dostepne dla uzytkownika jako obiekty wskazniki i input.
+#'
+#' @docType package
+#' @name eeval
+
+NULL
