a3f8b51c580c3e954e49f88e8fd639e6e29f0465
Jakub Rudek <rjakub999@gmail.com>
2020-11-26 22:21:10 +0100

    Uzupelnienie dokumentacji - osobno dla input i wskazniki

--- R/input.R
+++ R/input.R
@@ -1,19 +1,5 @@
 #' Dane potrzebne dla funkcji szacowania emisji
 #'
-#' @source Plik emisja2019.xlsx
-#' @format Ramka danych zawiera:
-#' \describe{
-#'  \item{Category}{Passenger Cars itd.}
-#'  \item{Fuel}{Petrol itd.}
-#' }
-#' @examples
-#' \dontrun{
-#'  wskazniki
-#'  input
-#' }
-"wskazniki"
-#' Dane potrzebne dla funkcji szacowania emisji
-#'
 #' @source Wylosowane dane
 #' @format Ramka danych zawiera:
 #' \describe{
